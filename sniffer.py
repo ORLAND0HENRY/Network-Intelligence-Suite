d9ed1995b5c9393f56275f41d0a1aa19edde8791
ORLAND0HENRY <icespice827@gmail.com>
2026-03-20 12:54:03 +0300

    Feat:added a DNS Resolver/Hostname Mapper

--- sniffer.py
+++ sniffer.py
@@ -11,18 +11,28 @@ FORBIDDEN_ZONES = ["142.250.188.46", "31.13.71.36"]
 
 EXCLUDE_IP = "192.x.x.x" # Enter ip to exclude from the scan e.g your own device
 
+import socket
+
+
+def get_hostname(ip):
+    try:
+        # This attempts to find the domain name associated with the IP
+        return socket.gethostbyaddr(ip)[0]
+    except socket.herror:
+        return "Unknown Host"
+
 
 def packet_callback(packet):
     if packet.haslayer(IP):
         src_ip = packet[IP].src
         dst_ip = packet[IP].dst
 
-        # IGNORE traffic coming TO your laptop/device
         if dst_ip == EXCLUDE_IP:
             return
 
-            # Now you ONLY see where the devices on network is trying to go out in the world
-        print(f"[OUTBOUND] {src_ip} is headed to -> {dst_ip}")
+        hostname = get_hostname(dst_ip)
 
-# We use the 'iface' parameter to lock onto your Intel Wireless card
-sniff(iface=conf.ifaces.dev_from_index(TARGET_IFACE), filter="ip", prn=packet_callback, store=0)
+        # Highlight if it hits a FORBIDDEN_ZONE
+        color = Fore.RED if dst_ip in FORBIDDEN_ZONES else Fore.GREEN
+
+        print(f"{color}[OUTBOUND]{Style.RESET_ALL} {src_ip} -> {dst_ip} ({Fore.CYAN}{hostname}{Style.RESET_ALL})")
