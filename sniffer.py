f1f6dd401e17a4208e8d573f2340a998b010c66b
ORLAND0HENRY <icespice827@gmail.com>
2026-03-21 21:30:11 +0300

    Fix:filter ip

--- sniffer.py
+++ sniffer.py
@@ -9,14 +9,14 @@ TARGET_IFACE = 24 #check wi-fi card interface and replace
 # Example: 142.250.188.46 is a Google IP (test it by pinging google)
 FORBIDDEN_ZONES = ["142.250.188.46", "31.13.71.36"]
 
-EXCLUDE_IP = "192.x.x.x" # Enter ip to exclude from the scan e.g your own device
+EXCLUDE_IP = "192.x.x.x" # Enter ip to exclude from the scan e.g. your own device
 
 import socket
 
 
 def get_hostname(ip):
     try:
-        # This attempts to find the domain name associated with the IP
+        # attempt to find the domain name associated with the IP
         return socket.gethostbyaddr(ip)[0]
     except socket.herror:
         return "Unknown Host"
